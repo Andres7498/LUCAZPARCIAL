1548edefa2847f9a7b44cb61e72b1ebc7032d8df
unknown <andres.martinez-v@labinfo.is.escuelaing.edu.co>
2023-10-26 13:01:51 -0500

    lucaz

--- demo/src/main/java/com/example/LucazService.java
+++ demo/src/main/java/com/example/LucazService.java
@@ -5,12 +5,12 @@ import static spark.Spark.*;
 public class LucazService {
 
     public static void main(String... args){
-        LucazSequence LucazSequence = new LucazSequence();
+        LucazSequence lucazSequence = new LucazSequence();
         port(getPort());
         staticFileLocation("/");
-        get("/lucazsequence", (req, res) -> {
+        get("/index", (req, res) -> {
             int value = Integer.parseInt(req.queryParams("value"));
-            String sequence = LucazSequence.calculateSequence(value);
+            String sequence = lucazSequence.calculateSequence(value);
             String response = 	"{\n" +
             "\n" +
             " \"operation\": \"lucazsequence\",\n" +
